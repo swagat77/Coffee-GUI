1949604b9e66946608f3b968f84e7e3cbb4b1073
student <swagat pandak>
2021-08-31 06:52:47 -0500

    full_credit

--- P01/full_credit/Hello.java
+++ P01/full_credit/Hello.java
@@ -1,5 +1,7 @@
-public class hello{
-    public static void main(String[] args){
-    System.out.println("Hello Swagat");
-    }
-} 
+ public class Hello
+{
+	public static void main(String[] args) {
+		System.out.println("Hello Swagat");
+	}
+}
+
