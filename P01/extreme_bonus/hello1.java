624d636dd3780f1d1fdb471a40613b2e378c4a21
swagat77 <77358070+swagat77@users.noreply.github.com>
2021-08-31 07:20:38 -0500

    Update hello1.java

--- P01/extreme_bonus/hello1.java
+++ P01/extreme_bonus/hello1.java
@@ -1,5 +1,5 @@
  import java.util.Scanner;
-public class hello1 {
+public class Hello {
 	public static void main(String[] args) {
 	  System.out.println(("Hello " +System.getProperty("user.name")+"!"));
 	  
