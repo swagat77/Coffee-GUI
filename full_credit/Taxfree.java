9c2e1032fc76c096727fc623b3b95e2ef04fba07
swagat77 <swagat.pandak@mavs.uta.edu>
2021-09-28 05:25:29 -0500

    full_credit

---
+++ full_credit/Taxfree.java
@@ -0,0 +1,13 @@
+ public class Taxfree{
+    public Taxfree(String name,Double cost){
+        name=name;
+        cost=unitCost;
+        
+    }
+    public void placeOrder(int quantity){
+        return quantity;
+    }
+    public void price(Double price){
+       return price; 
+    }
+}
