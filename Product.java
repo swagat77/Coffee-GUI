a07ffc22a1d4ec51c0bbbaadf567d56307ff441e
swagat77 <swagat.pandak@mavs.uta.edu>
2021-10-12 05:17:43 -0500

    full_credit

---
+++ Product.java
@@ -0,0 +1,20 @@
+abstract class Product{
+ protected String name;
+ protected double price;
+ protected double cost;
+ 
+ public Product extend(String name,double price, double cost){
+ this.name=name;
+ this.price=price;
+ this.cost=cost;
+ }
+ public String name(){
+ return name;}
+ @override
+ public  String toString (){
+ return name;
+ return cost;
+ return price;
+  }
+  } 
+ 
---
+++ Store.java
@@ -0,0 +1,42 @@
+public class Store1{
+protected String storeName;
+protected ArrayList<Prodduct> products;
+public Store(String storeName){
+this.storeName;
+}
+public storeName()
+return String;
+}
+public addProduct(Product product){
+return product;
+}
+public numberOfProduct()
+return int;
+}
+public String toString(int productindex){
+return product;
+}
+@override
+public String toString(){
+return return menu;
+
+}
+
+
+public static void main(String[] args){
+Scanner in=new Scanner(System.in);
+System.out.println("What is the name of the new wstore?");
+String a=in.nextString();
+System.out.println("Welcome to"+a);
+System.out.println("Options:);
+System.out.println("0) Exit\n1)Add java\n2)Add donut");
+System.out.println("Choice?");
+int b=in.nextInt;
+Donut a=new Donut();
+a.Donut();
+java b= new java();
+b.java();
+Product p= new Product();
+p.Product();
+
+
