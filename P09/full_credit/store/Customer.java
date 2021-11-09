560729cf75ba8de5b959fad94591fb52cb858fb3
swagat77 <77358070+swagat77@users.noreply.github.com>
2021-11-09 02:19:43 -0600

    Create Customer.java

---
+++ P09/full_credit/store/Customer.java
@@ -0,0 +1,22 @@
+package store;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+public class Customer extends Person{
+ public Customer(String name, String phone){
+ this.name=name;
+ this.phone=phone;
+ }
+ public Product(BufferedReader in) throws IOException {
+        this.name  = in.readLine();
+        this.phone = in.readLine();
+        
+    }
+       @Override
+    public void save(BufferedWriter out) throws IOException {
+        out.write("" + name  + '\n');
+        out.write("" + phone  + '\n');
+      
+    }
+ }
+       
