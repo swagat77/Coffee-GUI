285d7823a504c24229e5d714d48c1d477637b44e
swagat77 <77358070+swagat77@users.noreply.github.com>
2021-11-16 01:56:19 -0600

    Create Server.java

---
+++ P10/full_credit/store/Server.java
@@ -0,0 +1,21 @@
+package store;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+public class  Server extends Person{
+public Server( String name, String phone, String ssn){
+super(name,phone,ssn);
+}
+public Customer(BufferedReader in) throws IOException {
+        super(in);
+    }
+    @Override
+    public void save(BufferedWriter out) throws IOException {
+        out.write(ssn + '\n');
+        super.save(out);
+    }
+    private String ssn;
+}
+
+
