9c368bbdb6da28f40352e8210dd69fcea57aadd6
swagat77 <77358070+swagat77@users.noreply.github.com>
2021-11-16 01:50:56 -0600

    Update Customer.java

--- P10/full_credit/store/Customer.java
+++ P10/full_credit/store/Customer.java
@@ -1,21 +1,20 @@
-package store; //help from sprint 4
+package store;  //help from sprint 4
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-public class Customer extends Person{
- public Customer(String name, String phone){
- this.name=name;
- this.phone=phone;
- }
- public Product(BufferedReader in) throws IOException {
-        this.name  = in.readLine();
-        this.phone = in.readLine();
-        
+
+public class Customer extends Person {
+    public static final String ID = "store.Customer";
+    public Customer(String name, String phone) {
+        super(name, phone);
     }
-       @Override
+    public Customer(BufferedReader in) throws IOException {
+        super(in);
+    }
+    @Override
     public void save(BufferedWriter out) throws IOException {
-        out.write("" + name  + '\n');
-        out.write("" + phone  + '\n');
-      
+        out.write(ID + '\n');
+        super.save(out);
     }
- }
+}
